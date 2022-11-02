fd44653f0971fd462f302f25b49f14164f63a26d
0mni-gears515 <adamyi515@gmail.com>
2022-11-02 15:41:06 -0500

    Finish the logic for registerUser in the controller so that it'll create new user in MongoDB.

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,23 +1,51 @@
+const User = require('../models/user.model');
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs');
+
 // @desc   Allow people to register as user.
 // @route  /api/users
 // @access PUBLIC
-const registerUser = async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
 
     // Validation rules whether they have submitted a request with the required parameters.
     if(!name || !email || !password){
         res.status(400).json({
-            errorMessage: 'All parameters must be entered.'
+            error: 'All parameters must be entered.'
         })
-    } else {
-        res.status(201).json({
-            message: 'User has been created.'
-        })
+    } 
+
+    try {
+        // If validation is met, check to see if the user exist with MongoDB.
+        const foundUser = await User.findOne({ email });
+        if(foundUser){
+            return res.status(400).json({
+                error: 'User already exist.'
+            })
+        }
+
+        // ////////////////////////////////////////////////////////////////////////////////////
+        // // If user does not EXIST, then create user in MongoDB.
+        // 1. Make sure to encrypt the password before saving the user in the DB.
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        // 2. Create a new user.
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        const newUser = await User.create(user);
+        return res.status(201).json(newUser);
+
+    } catch (error) {
+        res.status(400);
     }
 
-    // If validation is met, check to see if the user exist with MongoDB.
-
-}
+});
 
 
 const loginUser = async (req, res) => {
